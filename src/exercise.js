87b23f1060280fb077f4f47c5d2a1512682d1c6b
Choilocal <hyunchoi@hist.co.kr>
2021-04-22 10:10:02 +0900

    action, reducer, dispatcher

---
+++ src/exercise.js
@@ -0,0 +1,79 @@
+import { createStore } from 'redux';
+// 스토어를 만들어주는 함수
+
+const initialState = {
+    counter: 0,
+    text: '',
+    list: []
+};
+
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
+
+
+const increase = () => ({
+    type: INCREASE,
+});
+
+const decrease = () => ({
+    type: DECREASE,
+});
+
+const changeTest = text => ({
+    type: CHANGE_TEXT,
+    text
+});
+
+const addToList = item => ({
+    type: ADD_TO_LIST,
+    item
+})
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + 1,
+            };
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1,
+            };
+        case CHANGE_TEXT:
+            return {
+                ...state,
+                text: action.text
+            };
+        case ADD_TO_LIST:
+            return {
+                ...state,
+                list: state.list.concat(action.item)
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+console.log(store.getState());
+
+const listener = () => {
+    const state = store.getState();
+    console.log(state);
+}
+
+const unsubscribe = store.subscribe(listener);
+// unsubscribe();
+
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeTest('안녕하세요'));
+store.dispatch(addToList({id: 1, text: '와우'}));
+
+window.store = store;
+
+window.unsubscribe = unsubscribe;
